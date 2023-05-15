f70959db14a0a2b174b60e0187ca0dfcf66e2adf
Toby Dodge <tobydodge65@gmail.com>
2023-05-15 13:58:04 -0230

    Added new stuff
    btruh

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
 # InternetRadio
 I have no other ideas for the name other than that, as it says what it does
 
-# What does it do?
+## What does it do?
 I got bored and wanted to host my own internet radio for a bit of fun, the software that is available for this isnt great
 so I am doing it myself.
 
-# What can I do with it?
+## What can I do with it?
 Eventually I want to implement it into a discord bot kinda like how I implemented other internet radios into my own discord bot
 
-# Timeline?
+## Timeline?
 I do not have a timeline at the moment as I am currently juggling multiple things but eventually things will work
+
+# Editing the software
+playlist.json is where you list the music you want to play
+
+```json
+{
+  "song_name": "path_to_song"
+}
+```
+
+This may look different depending on how you run the server
--- src/main.py
+++ src/main.py
@@ -1,7 +1,33 @@
-import hug
+import flask
+from flask import Flask, Response
+from flask_restful import Resource, Api, reqparse
 
-@hug.get()
-def hello():
-    """testing returning playlists"""
-    return "nothing rn"
+app = Flask(__name__)
+api = Api(app)
+parser = reqparse.RequestParser()
 
+
+@app.route('/')
+def home():
+    return flask.render_template('html/mainpage.html')
+
+
+'''
+class mainpage(Resource):
+    def get(self):
+        return flask.render_template('D:\Projects\InternetRadio\src\html\mainpage.html')
+
+
+@app.route("/music")
+def streamCountry():
+    def generate():
+        with open("music/Yousei Teikoku - Autoscopy.mp3", "rb") as country:
+            data = country.read(1024)
+            while data:
+                yield data
+                data = country.read(1024)
+    return Response(generate(), mimetype="audio/mp3")
+'''
+
+if __name__ == "__main__":
+    app.run(debug=True)
